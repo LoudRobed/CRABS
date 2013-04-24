4c859db45029d880ee9607b1ddd1eb594f46466e
merethhe <merethhe@gmail.com>
2013-04-24 15:43:20 +0200

    search, working on retrieval

--- controllers/swarm_controller/swarm_controller.c
+++ controllers/swarm_controller/swarm_controller.c
@@ -2,7 +2,7 @@
  * File:          swarm_controller.c
  * Date:          
  * Description:   
- * Author:        
+ * Author:        Andreas Hagen, Odd Andreas Sorsather
  * Modifications: 
  */
 
@@ -11,17 +11,25 @@
  * <webots/differential_wheels.h>, etc.
  */
 #include <webots/robot.h>
-
+#include "search.c"
+#include "stagnation.c"
+#include "retrieval.c"
 /*
  * You may want to add macros here.
  */
 #define TIME_STEP 64
-
+#define SEARCH_THRESH 250
+#define RETRIEVAL_THRESH 2400
+#define SEARCH_LAYER = 0
+#define STAGNATION_LAYER = 1
+#define RETRIEVAL_LAYER = 2
 /*
  * This is the main program.
  * The arguments of the main function can be specified by the
  * "controllerArgs" field of the Robot node
  */
+
+
 int main(int argc, char **argv)
 {
   /* necessary to initialize webots stuff */
@@ -33,28 +41,114 @@ int main(int argc, char **argv)
    *  WbDeviceTag my_sensor = wb_robot_get_device("my_sensor");
    *  WbDeviceTag my_actuator = wb_robot_get_device("my_actuator");
    */
+   
+  /*
+  * Most of this initialization code we found at http://www.cyberbotics.com/dvd/common/doc/webots/guide/section7.5.html
+  * Changed some of the names to make them more intiutive for ourselves.
+  *
+  */
   
+  WbDeviceTag ps[8];
+  char ps_names[8][4] = {
+    "ps0", "ps1", "ps2", "ps3",
+    "ps4", "ps5", "ps6", "ps7"
+  };
+  
+  // initialize devices
+  for (i=0; i<8 ; i++) {
+    ps[i] = wb_robot_get_device(ps_names[i]);
+    wb_distance_sensor_enable(ps[i], TIME_STEP);
+  }
+  
+  WbDeviceTag ls[8];
+  char ls_names[8][4] = {
+    "ls0", "ls1", "ls2", "ls3",
+    "ls4", "ls5", "ls6", "ls7"
+  };
+  
+  // initialize devices
+  for (i=0; i<8 ; i++) {
+    ls[i] = wb_robot_get_device(ls_names[i]);
+    wb_light_sensor_enable(ls[i], TIME_STEP);
+  }
+  
+  WbDeviceTag led[8];
+  char led_names[8][5] = {
+    "led0", "led1", "led2", "led3",
+    "led4", "led5", "led6", "led7"
+  };
+  
+  // initialize devices
+  for (i=0; i<8 ; i++) {
+    led[i] = wb_robot_get_device(led_names[i]);
+  }
   /* main loop
    * Perform simulation steps of TIME_STEP milliseconds
    * and leave the loop when the simulation is over
    */
   while (wb_robot_step(TIME_STEP) != -1) {
     
+	  
     /* 
      * Read the sensors :
      * Enter here functions to read sensor data, like:
      *  double val = wb_distance_sensor_get_value(my_sensor);
      */
+	int CONTROLLING_LAYER = 0;
+	double distance_sensors[8];
+	double light_sensors[8];
+	double retrieval_left_wheel_speed;
+    double retrieval_right_wheel_speed; 
+	double search_left_wheel_speed;
+	double search_right_wheel_speed;
+	double stagnation_left_wheel_speed;
+	double stagnation_right_wheel_speed;
+	int i;
+
+	//Getting data from the search layer
+	for(i=0; i<8; i++){
+		distance_sensors[i] = wb_distance_sensor_get_value(ps[i]);
+	}
+
+    update_search_speed(distance_sensors, SEARCH_THRESH);
+	search_left_wheel_speed = get_search_left_wheel_speed();
+	search_right_wheel_speed = get_search_right_wheel_speed();
+
+	//Getting data from the retrieval layer:
     
-    /* Process sensor data here */
+    for (i=0; i<8 ; i++){
+        light_sensors[i] = wb_light_sensor_get_value(ls[i]);
+      }
     
-    /*
-     * Enter here functions to send actuator commands, like:
-     * wb_differential_wheels_set_speed(100.0,100.0);
-     */
+    for(i=0; i<8; i++){
+      if(light_sensors[i] < RETRIEVAL_THRESH ){
+      swarm_retrieval(light_sensors, RETRIEVAL_THRESH);
+      retrieval_left_wheel_speed = get_retrieval_left_wheel_speed();
+      retrieval_right_wheel_speed = get_retrieval_right_wheel_speed();
+	  CONTROLLING_LAYER = 1;
+	  printf("Controlling layer set to stun!\n");
+	  break;
+      }
+    }
      
-     wb_differential_wheels_set_speed(100.0,100.0);
-     
+
+	if(CONTROLLING_LAYER==1){
+	     wb_differential_wheels_set_speed(retrieval_left_wheel_speed,retrieval_right_wheel_speed);
+               printf("RETRIEVING!\n");
+	}
+
+	else if(CONTROLLING_LAYER==2){    
+	
+     wb_differential_wheels_set_speed(stagnation_left_wheel_speed,stagnation_right_wheel_speed);
+
+	
+	}
+	else{
+		wb_differential_wheels_set_speed(search_left_wheel_speed,search_right_wheel_speed);
+		//printf("Hello World!\n");
+		
+	}
+
   };
   
   /* Enter your cleanup code here */
