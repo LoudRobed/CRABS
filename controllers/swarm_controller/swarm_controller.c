8a85cafcf165abe635e1486243f710cca4dd9750
merethhe <merethhe@gmail.com>
2013-04-25 18:09:10 +0200

    Litt mer omtrukturering

--- controllers/swarm_controller/swarm_controller.c
+++ controllers/swarm_controller/swarm_controller.c
@@ -126,8 +126,7 @@ int main(int argc, char **argv)
 
     update_search_speed(distance_sensors, SEARCH_THRESH);
    
-	search_left_wheel_speed = get_search_left_wheel_speed();
-    search_right_wheel_speed = get_search_right_wheel_speed();
+	
 
     //Send relevant sensor data to the retrieval layer 
     
@@ -150,8 +149,7 @@ int main(int argc, char **argv)
        }
     }
     
-	retrieval_left_wheel_speed = get_retrieval_left_wheel_speed();		
-    retrieval_right_wheel_speed = get_retrieval_right_wheel_speed();
+	
 
 	//Send relevant sensor data to the retrieval layer 
 
@@ -160,6 +158,12 @@ int main(int argc, char **argv)
 	if(stagnation){
 		CONTROLLING_LAYER=STAGNATION_LAYER;
 	}
+	
+	search_left_wheel_speed = get_search_left_wheel_speed();
+    search_right_wheel_speed = get_search_right_wheel_speed();
+	
+	retrieval_left_wheel_speed = get_retrieval_left_wheel_speed();		
+    retrieval_right_wheel_speed = get_retrieval_right_wheel_speed();
 	
 	stagnation_left_wheel_speed = get_stagnation_left_wheel_speed();
 	stagnation_right_wheel_speed = get_stagnation_right_wheel_speed();
